8ab5dcbe0b11a211e530c6def363021a0d21db18
Alec Gorge <alecgorge@gmail.com>
2010-10-04 21:09:54 -0400

    inital commit

---
+++ examples.php
@@ -0,0 +1,81 @@
+<?php
+// include the classes
+require "string.php";
+
+// a new "string"
+$a = new String("1234567890");
+
+// splits at each character and then outputs each character with its key.
+// output:
+/*
+0: 1
+1: 2
+2: 3
+3: 4
+4: 5
+5: 6
+6: 7
+7: 8
+8: 9
+9: 0
+*/
+$a->split()->each(function ($value, $key) {
+	echo "$key: $value\n";
+});
+
+// it works in foreach too.
+// this is gives the same output as above
+foreach($a->split() as $key => $value) {
+	echo "$key: $value\n";
+}
+
+// regex too!
+// output:
+/*
+Number found: 9
+Number found: 1000
+*/
+$b = new String("This 9 word sentence is less than 1000 letters long.");
+$b->match("/([0-9]+)/")->each(function ($value) {
+	echo "Number found: $value\n";
+});
+
+// replaces all numbers with NUMBER. Third arg must be true because last arg is a regular expression
+// output: This NUMBER word sentence is less than NUMBER letters long.
+echo $b->replace("/([0-9]+)/", "NUMBER", true)."\n";
+
+// not using regular expressions, and lowercase
+// output: this9wordsentenceislessthan1000letterslong.
+echo $b->replace(" ", "")->toLower()."\n";
+
+// something a bit more complex
+// strips the trailing 0's and 9's and splits it into an array, adds an element, reverse sorts it and turns it into an array
+// output:
+/*
+Array
+(
+    [8] => hi there!
+    [7] => 8
+    [6] => 7
+    [5] => 6
+    [4] => 5
+    [3] => 4
+    [2] => 3
+    [1] => 2
+    [0] => 1
+)
+*/
+print_r($a->rtrim('09')->split()->push('hi there!')->rsort()->toArray());
+
+
+// turning a standard array into a special array
+$c = new Arr(array('hi', 'there', 'how', 'are', 'you?'));
+
+// output: hi there how are you?
+echo $c->join(' ')."\n";
+
+// you can still use the String class like an array:
+$d = new String("012345");
+
+// output: string(1) "3"
+var_dump($d[3]);
---
+++ string.php
@@ -0,0 +1,393 @@
+<?php
+// why doesn't this function exist?
+if(!function_exists('mb_str_replace')) {
+	function mb_str_replace($search, $replace, $subject) {
+		if(is_array($subject)) {
+			$ret = array();
+			foreach($subject as $key => $val) {
+				$ret[$key] = mb_str_replace($search, $replace, $val);
+			}
+			return $ret;
+		}
+		foreach((array) $search as $key => $s) {
+			if($s == '') {
+				continue;
+			}
+			$r = !is_array($replace) ? $replace : (array_key_exists($key, $replace) ? $replace[$key] : '');
+			$pos = mb_strpos($subject, $s);
+			while($pos !== false) {
+				$subject = mb_substr($subject, 0, $pos) . $r . mb_substr($subject, $pos + mb_strlen($s));
+				$pos = mb_strpos($subject, $s, $pos + mb_strlen($r));
+			}
+		}
+		return $subject;
+	}
+}
+if(!function_exists('mb_str_split')) {
+	function mb_str_split($str, $length = 1) {
+		if ($length < 1) return FALSE;
+
+		$result = array();
+
+		for ($i = 0; $i < mb_strlen($str); $i += $length) {
+			$result[] = mb_substr($str, $i, $length);
+		}
+
+		return $result;
+	}
+}
+
+class StaticString {
+	/* static methods wrapping multibyte */
+
+	/**
+	 * Wrapper for substr
+	 */
+	public static function substr ($string, $start, $length = null) {
+		if(String::$multibyte) {
+			return new String(mb_substr($string, $start, $length, String::$multibyte_encoding));
+		}
+		else {
+			return new String(substr($string, $start, $length));
+		}
+	}
+
+	/**
+	 * Equivelent of Javascript's String.substring
+	 * @link http://www.w3schools.com/jsref/jsref_substring.asp
+	 */
+	public static function substring ($string, $start, $end) {
+		if(empty($length)) {
+			return self::substr($string, $start);
+		}
+		return self::substr($string, $end - $start);
+	}
+
+	public function charAt ($str, $point) {
+		return self::substr($str, $point, 1);
+	}
+
+	public function charCodeAt ($str, $point) {
+		return ord(self::substr($str, $point, 1));
+	}
+
+	public static function concat () {
+		$args = func_get_args();
+		$r = "";
+		foreach($args as $arg) {
+			$r .= (string)$arg;
+		}
+		return $arg;
+	}
+
+	public static function fromCharCode ($code) {
+		return chr($code);
+	}
+
+	public static function indexOf ($haystack, $needle, $offset = 0) {
+		if(String::$multibyte) {
+			return mb_strpos($haystack, $needle, $offset, String::$multibyte_encoding);
+		}
+		else {
+			return strpos($haystack, $needle, $offset);
+		}
+	}
+
+	public static function lastIndexOf ($haystack, $needle, $offset = 0) {
+		if(String::$multibyte) {
+			return mb_strrpos($haystack, $needle, $offset, String::$multibyte_encoding);
+		}
+		else {
+			return strrpos($haystack, $needle, $offset);
+		}
+	}
+
+	public static function match ($haystack, $regex) {
+		preg_match_all($regex, $haystack, $matches, PREG_PATTERN_ORDER);
+		return new Arr($matches[0]);
+	}
+
+	public static function replace ($haystack, $needle, $replace, $regex = false) {
+		if($regex) {
+			$r = preg_replace($needle, $replace, $haystack);
+		}
+		else {
+			if(String::$multibyte) {
+				$r = mb_str_replace($needle, $replace, $haystack);
+			}
+			else {
+				$r = str_replace($needle, $replace, $haystack);
+			}
+		}
+		return new String($r);
+	}
+
+	public static function strlen ($string) {
+		if(String::$multibyte) {
+			return mb_strlen($string, String::$multibyte_encoding);
+		}
+		else {
+			return strlen($string);
+		}
+	}
+
+	public static function slice ($string, $start, $end = null) {
+		return self::substring($string, $start, $end);
+	}
+
+	public static function toLowerCase ($string) {
+		if(String::$multibyte) {
+			return new String(mb_strtolower($string, String::$multibyte_encoding));
+		}
+		else {
+			return new String(strtolower($string));
+		}
+
+	}
+
+	public static function toUpperCase ($string) {
+		if(String::$multibyte) {
+			return new String(mb_strtoupper($string, String::$multibyte_encoding));
+		}
+		else {
+			return new STring(strtoupper($string));
+		}
+
+	}
+
+	public static function split ($string, $at = '') {
+		if(empty($at)) {
+			if(String::$multibyte) {
+				return new Arr(mb_str_split($string));
+			}
+			else {
+				return new Arr(str_split($string));
+			}
+		}
+		return new Arr(explode($at, $string));
+	}
+
+	/* end static wrapper methods */
+}
+class String implements ArrayAccess {
+	private $value;
+	
+	public static $multibyte = false;
+	private static $checked = false;
+	public static $multibyte_encoding = null;
+	
+	/* magic methods */
+	public function __construct ($string) {
+		if(!self::$checked) {
+			self::$multibyte = extension_loaded('mbstring');
+		}
+		if(is_null(self::$multibyte_encoding)) {
+			if(self::$multibyte) {
+				self::$multibyte_encoding = mb_internal_encoding();
+			}
+		}
+		$this->value = (string)$string;
+	}
+	
+	public function __toString () {
+		return $this->value;
+	}
+
+	/* end magic methods */
+	
+	/* ArrayAccess Methods */
+	
+	/** offsetExists ( mixed $index )
+		*
+		* Similar to array_key_exists
+		*/
+	public function offsetExists ($index) {
+		return !empty($this->value[$index]);
+	}
+	
+	/* offsetGet ( mixed $index ) 
+	 *
+	 * Retrieves an array value
+	 */
+	public function offsetGet ($index) {
+		return StaticString::substr($this->value, $index, 1)->toString();
+	}
+	
+	/* offsetSet ( mixed $index, mixed $val ) 
+	 *
+	 * Sets an array value
+	 */
+	public function offsetSet ($index, $val) {
+		$this->value = StaticString::substring($this->value, 0, $index) . $val . StaticString::substring($this->value, $index+1, StaticString::strlen($this->value));
+	}
+	
+	/* offsetUnset ( mixed $index ) 
+	 *
+	 * Removes an array value
+	 */
+	public function offsetUnset ($index) {
+		$this->value = StaticString::substr($this->value, 0, $index) . StaticString::substr($this->value, $index+1);
+	}
+
+	public static function create ($obj) {
+		if($obj instanceof String) return new String($obj);
+		return new String($obj);
+	}
+	
+	/* public methods */
+	public function substr ($start, $length) {
+		return StaticString::substr($this->value, $start, $length);
+	}
+
+	public function substring ($start, $end) {
+		return StaticString::substring($this->value, $start, $end);
+	}
+
+	public function charAt ($point) {
+		return StaticString::substr($this->value, $point, 1);
+	}
+
+	public function charCodeAt ($point) {
+		return ord(StaticString::substr($this->value, $point, 1));
+	}
+
+	public function indexOf ($needle, $offset) {
+		return StaticString::indexOf($this->value, $needle, $offset);
+	}
+
+	public function lastIndexOf ($needle) {
+		return StaticString::lastIndexOf($this->value, $needle);
+	}
+
+	public function match ($regex) {
+		return StaticString::match($this->value, $regex);
+	}
+
+	public function replace ($search, $replace, $regex = false) {
+		return StaticString::replace($this->value, $search, $replace, $regex);
+	}
+
+	public function first () {
+		return StaticString::substr($this->value, 0, 1);
+	}
+
+	public function last () {
+		return StaticString::substr($this->value, -1, 1);
+	}
+
+	public function search ($search, $offset = null) {
+		return $this->indexOf($search, $offset);
+	}
+
+	public function slice ($start, $end = null) {
+		return StaticString::slice($this->value, $start, $end);
+	}
+
+	public function toLowerCase () {
+		return StaticString::toLowerCase($this->value);
+	}
+
+	public function toUpperCase () {
+		return StaticString::toUpperCase($this->value);
+	}
+
+	public function toUpper () {
+		return $this->toUpperCase();
+	}
+
+	public function toLower () {
+		return $this->toLowerCase();
+	}
+
+	public function split ($at = '') {
+		return StaticString::split($this->value, $at);
+	}
+
+	public function trim ($charlist = null) {
+		return new String(trim($this->value, $charlist));
+	}
+
+	public function ltrim ($charlist = null) {
+		return new String(ltrim($this->value, $charlist));
+	}
+
+	public function rtrim ($charlist = null) {
+		return new String(rtrim($this->value, $charlist));
+	}
+
+	public function toString () {
+		return $this->__toString();
+	}
+}
+class Arr extends ArrayObject {
+	private static $ret_obj = true;
+
+	public function add () {
+		$val = 0;
+		foreach($this as $vals) {
+			$val += $vals;
+		}
+		return $val;
+	}
+
+	public function get ($i) {
+		$val = $this->offsetGet($i);
+		if(is_array($val)) {
+			return new self($val);
+		}
+		if(is_string($val) && self::$ret_obj) {
+			return new String($val);
+		}
+		return $val;
+	}
+
+	public function each ($callback) {
+		foreach($this as $key => $val) {
+			call_user_func_array($callback, array(
+				$val, $key, $this
+			));
+		}
+		return $this;
+	}
+
+	public function set ($i, $v) {
+		$this->offsetSet($i, $v);
+		return $this;
+	}
+
+	public function push ($value) {
+		$this[] = $value;
+		return $this;
+	}
+
+	public function join ($paste = '') {
+		return implode($paste, $this->getArrayCopy());
+	}
+
+	public function sort () {
+		$this->asort();
+		return $this;
+	}
+
+	public function toArray () {
+		return $this->getArrayCopy();
+	}
+
+	public function natsort () {
+		parent::natsort();
+		return $this;
+	}
+
+	public function rsort () {
+		parent::uasort('Arr::sort_alg');
+		return $this;
+	}
+
+	public static function sort_alg ($a,$b) {
+	    if ($a == $b) {
+			return 0;
+		}
+		return ($a < $b) ? 1 : -1;
+	}
+}
+

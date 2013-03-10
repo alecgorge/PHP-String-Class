1cc8598a82a0f8e402db64b5810f8192b10bd77c
Alec Gorge <alecgorge@gmail.com>
2013-03-10 20:59:55 +0100

    Replace `mb_str_replace` implementation with `preg_replace`
    https://gist.github.com/alecgorge/5130170

--- string.php
+++ string.php
@@ -2,26 +2,8 @@
 
 // why doesn't this function exist?
 if(!function_exists('mb_str_replace')) {
-	function mb_str_replace($search, $replace, $subject) {
-		if(is_array($subject)) {
-			$ret = array();
-			foreach($subject as $key => $val) {
-				$ret[$key] = mb_str_replace($search, $replace, $val);
-			}
-			return $ret;
-		}
-		foreach((array) $search as $key => $s) {
-			if($s == '') {
-				continue;
-			}
-			$r = !is_array($replace) ? $replace : (array_key_exists($key, $replace) ? $replace[$key] : '');
-			$pos = mb_strpos($subject, $s);
-			while($pos !== false) {
-				$subject = mb_substr($subject, 0, $pos) . $r . mb_substr($subject, $pos + mb_strlen($s));
-				$pos = mb_strpos($subject, $s, $pos + mb_strlen($r));
-			}
-		}
-		return $subject;
+	function mb_str_replace($search, $replace, $subject){
+		return preg_replace('@'.preg_quote($search).'@u',$replace,$subject);
 	}
 }
 if(!function_exists('mb_str_split')) {
